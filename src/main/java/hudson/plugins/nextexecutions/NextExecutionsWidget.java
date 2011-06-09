b6b0f6b4498758af52f05d5f15997854fda282c6
Ignacio Albors <ignacio@albors.ws>
2011-06-09 12:39:38 +0200

    Multiline crons supported.

--- src/main/java/hudson/plugins/nextexecutions/NextExecutionsWidget.java
+++ src/main/java/hudson/plugins/nextexecutions/NextExecutionsWidget.java
@@ -29,20 +29,38 @@ public class NextExecutionsWidget extends Widget {
 		for (AbstractProject project: l) {
 			Trigger trigger = project.getTrigger(TimerTrigger.class);
 			if(trigger != null){
-				try {
-					CronTab c = new CronTab(trigger.getSpec());
+				List<CronTab> crons = parseSpec(trigger.getSpec());
+				Calendar cal = null;
+				for (CronTab cronTab : crons) {
 					Date d = new Date();				
-					Calendar cal = c.ceil(d.getTime());
-					nblist.add(new NextBuilds(project.getName(), cal));
-
-				} catch (ANTLRException e) {
-
-					e.printStackTrace();
+					cal = (cal == null || cal.compareTo(cronTab.ceil(d.getTime())) > 0)? cronTab.ceil(d.getTime()) : cal;					
 				}
+				if(cal != null)
+					nblist.add(new NextBuilds(project.getName(), cal));
 			}
 		}
 		Collections.sort(nblist);
 		return nblist;
 		
+	}
+	
+	/**
+	 * Pretty much the same as {@link CronTabList#create(String)}
+	 */
+	private List<CronTab> parseSpec(String format) {
+		Vector<CronTab> r = new Vector<CronTab>();
+        int lineNumber = 0;
+        for (String line : format.split("\\r?\\n")) {
+            lineNumber++;
+            line = line.trim();
+            if(line.length()==0 || line.startsWith("#"))
+                continue;   // ignorable line
+            try {
+                r.add(new CronTab(line,lineNumber));
+            } catch (ANTLRException e) {
+                e.printStackTrace();
+            }
+        }
+        return r;
 	}
 }
