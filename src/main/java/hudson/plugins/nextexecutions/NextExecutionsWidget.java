9c03772fca5cf0196d5c45b2d8f1942a875c347d
Ignacio Albors <ignacio@albors.ws>
2011-06-15 10:05:20 +0200

    Show only the jobs associated with the view.

--- src/main/java/hudson/plugins/nextexecutions/NextExecutionsWidget.java
+++ src/main/java/hudson/plugins/nextexecutions/NextExecutionsWidget.java
@@ -1,17 +1,22 @@
 package hudson.plugins.nextexecutions;
 
 import java.util.Calendar;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.Vector;
 import java.util.logging.Logger;
 
+import org.kohsuke.stapler.Stapler;
+
 import antlr.ANTLRException;
 
 import hudson.Extension;
 import hudson.model.AbstractProject;
 import hudson.model.Hudson;
+import hudson.model.View;
+import hudson.model.TopLevelItem;
 import hudson.scheduler.CronTab;
 import hudson.triggers.TimerTrigger;
 import hudson.triggers.Trigger;
@@ -23,8 +28,25 @@ public class NextExecutionsWidget extends Widget {
 	 
 	public List<NextBuilds> getBuilds() {		
 		List<NextBuilds> nblist = new Vector<NextBuilds>();
+
+		List<AbstractProject> l;
 		
-		List<AbstractProject> l = Hudson.getInstance().getItems(AbstractProject.class);
+		View v = Stapler.getCurrentRequest().findAncestorObject(View.class);
+		
+		if(v != null) {
+			Collection<TopLevelItem> tli = v.getItems();
+			Vector<AbstractProject> vector = new Vector<AbstractProject>();
+			for (TopLevelItem topLevelItem : tli) {
+				if(topLevelItem instanceof AbstractProject){
+					vector.add((AbstractProject)topLevelItem);
+				}
+			}
+			l = vector;
+		}
+		else{
+			l = Hudson.getInstance().getItems(AbstractProject.class); 
+		}
+			
 		
 		for (AbstractProject project: l) {
 			if(!project.isDisabled()){
