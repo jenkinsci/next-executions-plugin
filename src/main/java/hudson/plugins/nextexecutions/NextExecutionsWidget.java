69982cb373a601039da61fd456c8993d506cb649
ialbors <ignacio@albors.ws>
2011-06-16 20:50:56 +0200

    Moved logic to an external class.

--- src/main/java/hudson/plugins/nextexecutions/NextExecutionsWidget.java
+++ src/main/java/hudson/plugins/nextexecutions/NextExecutionsWidget.java
@@ -17,6 +17,7 @@ import hudson.model.AbstractProject;
 import hudson.model.Hudson;
 import hudson.model.View;
 import hudson.model.TopLevelItem;
+import hudson.plugins.nextexecutions.utils.NextExecutionsUtils;
 import hudson.scheduler.CronTab;
 import hudson.triggers.TimerTrigger;
 import hudson.triggers.Trigger;
@@ -49,42 +50,14 @@ public class NextExecutionsWidget extends Widget {
 			
 		
 		for (AbstractProject project: l) {
-			if(!project.isDisabled()){
-				Trigger trigger = project.getTrigger(TimerTrigger.class);
-				if(trigger != null){
-					List<CronTab> crons = parseSpec(trigger.getSpec());
-					Calendar cal = null;
-					for (CronTab cronTab : crons) {
-						Date d = new Date();				
-						cal = (cal == null || cal.compareTo(cronTab.ceil(d.getTime())) > 0)? cronTab.ceil(d.getTime()) : cal;					
-					}
-					if(cal != null)
-						nblist.add(new NextBuilds(project, cal));
-				}
-			}
+			NextBuilds nb = NextExecutionsUtils.getNextBuild(project);
+			if(nb != null)
+				nblist.add(nb);
 		}
 		Collections.sort(nblist);
 		return nblist;
 		
 	}
 	
-	/**
-	 * Pretty much the same as {@link CronTabList#create(String)}
-	 */
-	private List<CronTab> parseSpec(String format) {
-		Vector<CronTab> r = new Vector<CronTab>();
-        int lineNumber = 0;
-        for (String line : format.split("\\r?\\n")) {
-            lineNumber++;
-            line = line.trim();
-            if(line.length()==0 || line.startsWith("#"))
-                continue;   // ignorable line
-            try {
-                r.add(new CronTab(line,lineNumber));
-            } catch (ANTLRException e) {
-                e.printStackTrace();
-            }
-        }
-        return r;
-	}
+
 }
---
+++ src/main/java/hudson/plugins/nextexecutions/utils/NextExecutionsUtils.java
@@ -0,0 +1,54 @@
+package hudson.plugins.nextexecutions.utils;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Vector;
+
+import antlr.ANTLRException;
+
+import hudson.model.AbstractProject;
+import hudson.plugins.nextexecutions.NextBuilds;
+import hudson.scheduler.CronTab;
+import hudson.triggers.TimerTrigger;
+import hudson.triggers.Trigger;
+
+public class NextExecutionsUtils {
+
+	public static NextBuilds getNextBuild(AbstractProject project){
+		if(!project.isDisabled()){
+			Trigger trigger = project.getTrigger(TimerTrigger.class);
+			if(trigger != null){
+				List<CronTab> crons = parseSpec(trigger.getSpec());
+				Calendar cal = null;
+				for (CronTab cronTab : crons) {
+					Date d = new Date();				
+					cal = (cal == null || cal.compareTo(cronTab.ceil(d.getTime())) > 0)? cronTab.ceil(d.getTime()) : cal;					
+				}
+				if(cal != null)
+					return new NextBuilds(project, cal);
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * Pretty much the same as {@link CronTabList#create(String)}
+	 */
+	private static List<CronTab> parseSpec(String format) {
+		Vector<CronTab> r = new Vector<CronTab>();
+        int lineNumber = 0;
+        for (String line : format.split("\\r?\\n")) {
+            lineNumber++;
+            line = line.trim();
+            if(line.length()==0 || line.startsWith("#"))
+                continue;   // ignorable line
+            try {
+                r.add(new CronTab(line,lineNumber));
+            } catch (ANTLRException e) {
+                e.printStackTrace();
+            }
+        }
+        return r;
+	}
+}
