d1c8552dae290b38cbf193ef8fbaabfa8240d0b9
Ignacio Albors <ignacio@albors.ws>
2014-05-30 14:24:50 +0200

    This Widget will show schedules for SCMTrigger (polling schedules).

--- src/main/java/hudson/plugins/nextexecutions/PossibleNextExecutionsWidget.java
+++ src/main/java/hudson/plugins/nextexecutions/PossibleNextExecutionsWidget.java
@@ -1,10 +1,15 @@
 package hudson.plugins.nextexecutions;
 
 import hudson.Extension;
+import hudson.triggers.SCMTrigger;
 
 @Extension
 public class PossibleNextExecutionsWidget extends NextExecutionsWidget {
 
+	public PossibleNextExecutionsWidget() {
+		triggerClass = SCMTrigger.class;
+	}
+	
 	@Override
 	public String getWidgetName() {
 		return Messages.PossibleNextExec_WidgetName();
