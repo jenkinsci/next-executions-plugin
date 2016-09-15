db5b5fe07d673eff01f1d9874b5e0586b2da8440
Ignacio Albors <ignacio@albors.ws>
2016-09-15 08:14:24 +0200

    Formatting.

--- src/main/java/hudson/plugins/nextexecutions/utils/NextExecutionsUtils.java
+++ src/main/java/hudson/plugins/nextexecutions/utils/NextExecutionsUtils.java
@@ -19,56 +19,53 @@ import jenkins.model.ParameterizedJobMixIn;
 
 public class NextExecutionsUtils {
 
-	/**
-	 * Returns the {@link NextBuilds} for the project.
-	 *
-	 * @return The {@link NextBuilds} object with the associated
-	 * next execution date or null.
-	 */
-	public static NextBuilds getNextBuild(ParameterizedJobMixIn.ParameterizedJob project){
-		return getNextBuild(project, TimerTrigger.class);
-	}
-	
-	public static NextBuilds getNextBuild(ParameterizedJobMixIn.ParameterizedJob project, Class<? extends Trigger> triggerClass){
-            Calendar cal = null;
-            // Only AbstractProject has isDisabled method
-            if((project instanceof AbstractProject && !((AbstractProject)project).isDisabled()) 
-                        || !(project instanceof AbstractProject)){
-                        Map<TriggerDescriptor,Trigger<?>> triggers = project.getTriggers();
-                        Iterator<Map.Entry<TriggerDescriptor,Trigger<?>>> iterator = triggers.entrySet().iterator();
-                        while(iterator.hasNext()) {
-                            Trigger trigger = iterator.next().getValue();
-                            if(trigger.getClass().equals(triggerClass)) {
-				try{
-				Field triggerTabsField = Trigger.class.getDeclaredField("tabs");
-				triggerTabsField.setAccessible(true);
-				
-				CronTabList cronTabList = (CronTabList)triggerTabsField.get(trigger);
-				
-				Field crontablistTabsField = CronTabList.class.getDeclaredField("tabs");
-				crontablistTabsField.setAccessible(true);
-				
-				List<CronTab> crons = (Vector<CronTab>)crontablistTabsField.get(cronTabList);
-				
-				for (CronTab cronTab : crons) {
-					Date d = new Date();
-					cal = (cal == null || cal.compareTo(cronTab.ceil(d.getTime())) > 0)? cronTab.ceil(d.getTime()) : cal;
-				}
-				}
-				catch(NoSuchFieldException e){
-					e.printStackTrace();
-				}
-				catch(IllegalAccessException e){
-					e.printStackTrace();
-				}
-                            }
+    /**
+     * Returns the {@link NextBuilds} for the project.
+     *
+     * @return The {@link NextBuilds} object with the associated next execution
+     * date or null.
+     */
+    public static NextBuilds getNextBuild(ParameterizedJobMixIn.ParameterizedJob project) {
+        return getNextBuild(project, TimerTrigger.class);
+    }
+
+    public static NextBuilds getNextBuild(ParameterizedJobMixIn.ParameterizedJob project, Class<? extends Trigger> triggerClass) {
+        Calendar cal = null;
+        // Only AbstractProject has isDisabled method
+        if ((project instanceof AbstractProject && !((AbstractProject) project).isDisabled())
+                || !(project instanceof AbstractProject)) {
+            Map<TriggerDescriptor, Trigger<?>> triggers = project.getTriggers();
+            Iterator<Map.Entry<TriggerDescriptor, Trigger<?>>> iterator = triggers.entrySet().iterator();
+            while (iterator.hasNext()) {
+                Trigger trigger = iterator.next().getValue();
+                if (trigger.getClass().equals(triggerClass)) {
+                    try {
+                        Field triggerTabsField = Trigger.class.getDeclaredField("tabs");
+                        triggerTabsField.setAccessible(true);
+
+                        CronTabList cronTabList = (CronTabList) triggerTabsField.get(trigger);
+
+                        Field crontablistTabsField = CronTabList.class.getDeclaredField("tabs");
+                        crontablistTabsField.setAccessible(true);
+
+                        List<CronTab> crons = (Vector<CronTab>) crontablistTabsField.get(cronTabList);
+
+                        for (CronTab cronTab : crons) {
+                            Date d = new Date();
+                            cal = (cal == null || cal.compareTo(cronTab.ceil(d.getTime())) > 0) ? cronTab.ceil(d.getTime()) : cal;
                         }
-		}
-                if(cal != null) {
-                    return new NextBuilds(project, cal);
+                    } catch (NoSuchFieldException e) {
+                        e.printStackTrace();
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    }
                 }
-                else {
-                    return null;
-                }
-	}
+            }
+        }
+        if (cal != null) {
+            return new NextBuilds(project, cal);
+        } else {
+            return null;
+        }
+    }
 }
