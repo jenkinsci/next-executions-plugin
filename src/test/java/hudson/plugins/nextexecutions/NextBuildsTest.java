8d15be33cd1b69e1bc47efc8cb9b83033eec3fca
Debayan Ghosh <debayanghosh852@gmail.com>
2024-01-09 12:55:28 +0530

    Edited testGetCorrectDate for custom date formats and added new testGetName

--- src/test/java/hudson/plugins/nextexecutions/NextBuildsTest.java
+++ src/test/java/hudson/plugins/nextexecutions/NextBuildsTest.java
@@ -28,6 +28,26 @@ public class NextBuildsTest {
             }
             NextBuilds nextBuilds = new NextBuilds(project, Calendar.getInstance());
             assertEquals(zdt.format(DateTimeFormatter.ofPattern(format)), nextBuilds.getDate());
+            descriptor.setDateFormat("yyyy-MM-dd HH:mm:ss");
+            format = descriptor.getDateFormat();
+            assertEquals(zdt.format(DateTimeFormatter.ofPattern(format)), nextBuilds.getDate());
+            j.jenkins.getQueue().clear();
+        } catch (Exception e) {
+            fail(e.toString());
+        }
+    }
+
+    @Test
+    public void testGetName() {
+        try {
+            FreeStyleProject project = j.createFreeStyleProject("test");
+            NextBuilds nextBuilds1 = new NextBuilds(project, Calendar.getInstance());
+            assertEquals("test", nextBuilds1.getName());
+            assertEquals("test", nextBuilds1.getshortName());
+            project.setDisplayName("Freestyle Project using Maven TEST");
+            NextBuilds nextBuilds2 = new NextBuilds(project, Calendar.getInstance());
+            assertEquals("Freestyle Project using Maven TEST", nextBuilds2.getName());
+            assertEquals("Freestyle Project u...", nextBuilds2.getshortName());
             j.jenkins.getQueue().clear();
         } catch (Exception e) {
             fail(e.toString());
