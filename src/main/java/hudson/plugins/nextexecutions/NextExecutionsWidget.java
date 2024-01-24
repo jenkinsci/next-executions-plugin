0d1683ca81d31f841608257ac76442462bb1a3d4
l3002 <tanmaymathpal4545@gmail.com>
2024-01-25 04:43:52 +0530

    modified: Next Execution Widget UI

--- src/main/java/hudson/plugins/nextexecutions/NextExecutionsWidget.java
+++ src/main/java/hudson/plugins/nextexecutions/NextExecutionsWidget.java
@@ -41,132 +41,135 @@ import org.kohsuke.stapler.export.ExportedBean;
 @SuppressWarnings("rawtypes")
 public class NextExecutionsWidget extends Widget {
 
-    @NonNull
-    private final String ownerUrl;
+  @NonNull
+  private final String ownerUrl;
 
-    private Class<? extends Trigger> triggerClass;
+  private Class<? extends Trigger> triggerClass;
 
-    public NextExecutionsWidget(@NonNull String ownerUrl) {
-        this(ownerUrl, TimerTrigger.class);
+  public NextExecutionsWidget(@NonNull String ownerUrl) {
+    this(ownerUrl, TimerTrigger.class);
+  }
+
+  public NextExecutionsWidget(@NonNull String ownerUrl, Class<? extends Trigger> triggerClass) {
+    this.ownerUrl = ownerUrl;
+    this.triggerClass = triggerClass;
+  }
+
+  @Override
+  public String getOwnerUrl() {
+    return ownerUrl;
+  }
+
+  @Exported(name = "next_executions")
+  public List<NextBuilds> getBuilds() {
+    List<NextBuilds> nblist = new Vector<NextBuilds>();
+
+    List<ParameterizedJobMixIn.ParameterizedJob> l;
+
+    View v = Stapler.getCurrentRequest().findAncestorObject(View.class);
+
+    Jenkins j = Jenkins.getInstanceOrNull();
+
+    if (j == null) {
+      return null;
     }
 
-    public NextExecutionsWidget(@NonNull String ownerUrl, Class<? extends Trigger> triggerClass) {
-        this.ownerUrl = ownerUrl;
-        this.triggerClass = triggerClass;
+    DescriptorImpl d = (DescriptorImpl) (j.getDescriptorOrDie(NextBuilds.class));
+
+    if (d.getFilterByView() && v != null) {
+      Collection<TopLevelItem> tli = v.getItems();
+      Vector<ParameterizedJobMixIn.ParameterizedJob> vector = new Vector<ParameterizedJobMixIn.ParameterizedJob>();
+      for (TopLevelItem topLevelItem : tli) {
+        if (topLevelItem instanceof ParameterizedJobMixIn.ParameterizedJob) {
+          vector.add((ParameterizedJobMixIn.ParameterizedJob) topLevelItem);
+        }
+      }
+      l = vector;
+    } else {
+      l = j.getAllItems(ParameterizedJobMixIn.ParameterizedJob.class);
+    }
+
+    for (ParameterizedJobMixIn.ParameterizedJob project : l) {
+      NextBuilds nb = NextExecutionsUtils.getNextBuild(project, triggerClass);
+      if (nb != null) {
+        nblist.add(nb);
+      }
+      // Check parameterized
+      else if (getShowParameterizedWidget()) {
+        nb = ParameterizedNextExecutionsUtils.getNextBuild(project, triggerClass);
+        if (nb != null) {
+          nblist.add(nb);
+        }
+      }
+    }
+
+    // Get also the items in the queue but only those that have a waiting
+    // period of more than a minute.
+
+    // Only for this class. Not its children
+    if (this.getClass() == NextExecutionsWidget.class) {
+      Item[] queueItems = Queue.getInstance().getItems();
+      for (Item item : queueItems) {
+        if (item instanceof WaitingItem && item.task instanceof ParameterizedJobMixIn.ParameterizedJob) {
+          WaitingItem waitingItem = (WaitingItem) item;
+          Calendar now = Calendar.getInstance();
+          long nowMilliseconds = now.getTimeInMillis();
+          now.setTimeInMillis(nowMilliseconds + 60 * 1000);
+          if (waitingItem.timestamp.after(now)) {
+            NextBuilds nb = new NextBuilds(
+                (ParameterizedJobMixIn.ParameterizedJob) item.task, waitingItem.timestamp);
+            nblist.add(nb);
+          }
+        }
+      }
+    }
+
+    Collections.sort(nblist);
+    return nblist;
+  }
+
+  public String getWidgetEmptyMessage() {
+    return Messages.NextExec_EmptyWidgetMessage();
+  }
+
+  public String getWidgetName() {
+    return Messages.NextExec_WidgetName();
+  }
+
+  public String getWidgetId() {
+    return "next-exec";
+  }
+
+  public boolean showWidget() {
+    return true;
+  }
+
+  public boolean getShowParameterizedWidget() {
+    Jenkins j = Jenkins.getInstanceOrNull();
+    DescriptorImpl d = j != null ? (DescriptorImpl) (j.getDescriptorOrDie(NextBuilds.class)) : null;
+    if (d == null) {
+      return false;
+    }
+    return d.getShowParameterizedWidget();
+  }
+
+  @Symbol("nextExecutionsWidget")
+  @Extension
+  public static final class FactoryImpl extends WidgetFactory<View, NextExecutionsWidget> {
+    @Override
+    public Class<View> type() {
+      return View.class;
     }
 
     @Override
-    public String getOwnerUrl() {
-        return ownerUrl;
+    public Class<NextExecutionsWidget> widgetType() {
+      return NextExecutionsWidget.class;
     }
 
-    @Exported(name = "next_executions")
-    public List<NextBuilds> getBuilds() {
-        List<NextBuilds> nblist = new Vector<NextBuilds>();
-
-        List<ParameterizedJobMixIn.ParameterizedJob> l;
-
-        View v = Stapler.getCurrentRequest().findAncestorObject(View.class);
-
-        Jenkins j = Jenkins.getInstanceOrNull();
-
-        if (j == null) {
-            return null;
-        }
-
-        DescriptorImpl d = (DescriptorImpl) (j.getDescriptorOrDie(NextBuilds.class));
-
-        if (d.getFilterByView() && v != null) {
-            Collection<TopLevelItem> tli = v.getItems();
-            Vector<ParameterizedJobMixIn.ParameterizedJob> vector =
-                    new Vector<ParameterizedJobMixIn.ParameterizedJob>();
-            for (TopLevelItem topLevelItem : tli) {
-                if (topLevelItem instanceof ParameterizedJobMixIn.ParameterizedJob) {
-                    vector.add((ParameterizedJobMixIn.ParameterizedJob) topLevelItem);
-                }
-            }
-            l = vector;
-        } else {
-            l = j.getAllItems(ParameterizedJobMixIn.ParameterizedJob.class);
-        }
-
-        for (ParameterizedJobMixIn.ParameterizedJob project : l) {
-            NextBuilds nb = NextExecutionsUtils.getNextBuild(project, triggerClass);
-            if (nb != null) {
-                nblist.add(nb);
-            }
-            // Check parameterized
-            else if (getShowParameterizedWidget()) {
-                nb = ParameterizedNextExecutionsUtils.getNextBuild(project, triggerClass);
-                if (nb != null) {
-                    nblist.add(nb);
-                }
-            }
-        }
-
-        // Get also the items in the queue but only those that have a waiting
-        // period of more than a minute.
-
-        // Only for this class. Not its children
-        if (this.getClass() == NextExecutionsWidget.class) {
-            Item[] queueItems = Queue.getInstance().getItems();
-            for (Item item : queueItems) {
-                if (item instanceof WaitingItem && item.task instanceof ParameterizedJobMixIn.ParameterizedJob) {
-                    WaitingItem waitingItem = (WaitingItem) item;
-                    Calendar now = Calendar.getInstance();
-                    long nowMilliseconds = now.getTimeInMillis();
-                    now.setTimeInMillis(nowMilliseconds + 60 * 1000);
-                    if (waitingItem.timestamp.after(now)) {
-                        NextBuilds nb = new NextBuilds(
-                                (ParameterizedJobMixIn.ParameterizedJob) item.task, waitingItem.timestamp);
-                        nblist.add(nb);
-                    }
-                }
-            }
-        }
-
-        Collections.sort(nblist);
-        return nblist;
+    @NonNull
+    @Override
+    public Collection<NextExecutionsWidget> createFor(@NonNull View target) {
+      return List.of(new NextExecutionsWidget(target.getUrl()));
     }
-
-    public String getWidgetName() {
-        return Messages.NextExec_WidgetName();
-    }
-
-    public String getWidgetId() {
-        return "next-exec";
-    }
-
-    public boolean showWidget() {
-        return true;
-    }
-
-    public boolean getShowParameterizedWidget() {
-        Jenkins j = Jenkins.getInstanceOrNull();
-        DescriptorImpl d = j != null ? (DescriptorImpl) (j.getDescriptorOrDie(NextBuilds.class)) : null;
-        if (d == null) {
-            return false;
-        }
-        return d.getShowParameterizedWidget();
-    }
-
-    @Symbol("nextExecutionsWidget")
-    @Extension
-    public static final class FactoryImpl extends WidgetFactory<View, NextExecutionsWidget> {
-        @Override
-        public Class<View> type() {
-            return View.class;
-        }
-
-        @Override
-        public Class<NextExecutionsWidget> widgetType() {
-            return NextExecutionsWidget.class;
-        }
-
-        @NonNull
-        @Override
-        public Collection<NextExecutionsWidget> createFor(@NonNull View target) {
-            return List.of(new NextExecutionsWidget(target.getUrl()));
-        }
-    }
+  }
 }
